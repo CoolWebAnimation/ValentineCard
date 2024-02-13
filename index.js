de3959c8df0d81633aabb09166b43ebabbbe7300
LazyYuuki <huy2840@gmail.com>
2024-02-14 00:22:51 +0700

    Update

--- index.js
+++ index.js
@@ -9,3 +9,16 @@ $(document).ready(function () {
     }, 'slow');
   });
 });
+
+let no = document.getElementById('no');
+
+no.addEventListener("mouseover", function (e) {
+  no.style = `left: ${Math.random() * 80}vw;`
+  console.log(no.style)
+})
+
+let yes = document.getElementById('yes');
+
+yes.addEventListener("click", function (e) {
+  alert("YAYYYYYYYYY!!!!!!!")
+})
